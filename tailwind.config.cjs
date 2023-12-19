db5deb26aec46a8cfb4db29344215937bbe95a13
Markus Kaihola <markus@makeri89.com>
2023-12-19 14:45:43 +0200

    Update the page content completely

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,19 @@ module.exports = {
 			},
 			boxShadow: {
 				card: '0px 5px 8px 1px #64748b'
-			}
+			},
+			backgroundImage: {
+				forest: 'linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5) ), url("forest.jpg")'
+			},
+			colors: {
+				carrot: '#f59627',
+				banana: '#dec06f',
+				leaf: '#1e400b',
+				rock: '#576663',
+				beet: '#72394d',
+				salsify: '#282828',
+				parsnip: '#f1ebe5',
+			},
 		},
 	},
 	plugins: [],
