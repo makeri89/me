f258f9724bff2c967fb438712c30d2cce29c0fb6
Markus Kaihola <markus@makeri89.com>
2023-12-19 23:55:22 +0200

    Run linting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				nunito: '"Nunito Sans Variable"'
-			},
-			boxShadow: {
-				card: '0px 5px 8px 1px #64748b'
-			},
-			backgroundImage: {
-				forest: 'linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5) ), url("/forest.jpg")'
-			},
-			colors: {
-				carrot: '#f59627',
-				banana: '#dec06f',
-				leaf: '#1e400b',
-				rock: '#576663',
-				beet: '#72394d',
-				salsify: '#282828',
-				parsnip: '#f1ebe5',
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: '"Nunito Sans Variable"',
+      },
+      boxShadow: {
+        card: "0px 5px 8px 1px #64748b",
+      },
+      backgroundImage: {
+        forest:
+          'linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5) ), url("/forest.jpg")',
+      },
+      colors: {
+        carrot: "#f59627",
+        banana: "#dec06f",
+        leaf: "#1e400b",
+        rock: "#576663",
+        beet: "#72394d",
+        salsify: "#282828",
+        parsnip: "#f1ebe5",
+      },
+    },
+  },
+  plugins: [],
+};
